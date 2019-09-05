b1da53e908d521ea3b677bec047d4e4663de2951
Muhammet KIRAN <43640752+muhammetkrn@users.noreply.github.com>
2019-09-05 10:03:04 +0300

    Update CodeRun.sh

--- CodeRun.sh
+++ CodeRun.sh
@@ -2,6 +2,6 @@
 while true; do
     python3 bash.py
 
-    curl -i -XPOST "http://35.198.129.164:8086/write?db=mydb" --data-binary @data.txt
+    curl -i -XPOST "http://xxx.xxx.xxx.xxx:8086/write?db=mydb" --data-binary @data.txt
     sleep 5s
 done
