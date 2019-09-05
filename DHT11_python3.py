400484e8af6fd5f0a0a43d6e0d3e1c68360f64b8
Muhammet KIRAN <43640752+muhammetkrn@users.noreply.github.com>
2019-09-05 10:03:38 +0300

    Update DHT11_python3.py

--- DHT11_python3.py
+++ DHT11_python3.py
@@ -10,5 +10,5 @@ while True:
                 print ('Temp: '+ str(temp) + '*C' + '\tHumidity:' + ' %'+ str(hum) + ' ' + str(time.strftime("%s",time.gmtime())))
                 time.sleep(1)
                 data.write('temperature,' + 'temp='+ str(temp) + ' ' + 'hum=' +str(hum) + ' ' + str(time.strftime("%s",time.gmtime())))
-                response = requests.post('http://35.198.129.164:8086/write?db=yeni4', data=f'temperature temp={temp} \n temperature hum={hum}')
+                response = requests.post('http://xxx.xxx.xxx.xxx:8086/write?db=yeni4', data=f'temperature temp={temp} \n temperature hum={hum}')
                 data.write('\n')
